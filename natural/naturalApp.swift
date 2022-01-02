3efedab1a63a6d75bd786bd742c0f02b98bb3f30
tylerj <tylerjcagle@gmail.com>
2022-01-01 21:24:47 -0700

    Initial Commit

---
+++ natural/naturalApp.swift
@@ -0,0 +1,17 @@
+//
+//  naturalApp.swift
+//  natural
+//
+//  Created by Tyler Cagle on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct naturalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
